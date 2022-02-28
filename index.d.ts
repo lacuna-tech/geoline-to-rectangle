f1f4f002c8d0219fbfe61f0f4dbf97a016adbe9a
ethan sherr <>
2022-02-28 13:30:57 -0500

    adding types

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,8 @@
 export module '@lacuna/geoline-to-rectangle' {
 
   // array of array of array of numbers,... lng, lat
-  declare type CoordinatesArray = Array<Array<Array<number>>>
+  declare type GepointArray = Array<Array<number>>
 
-  declare class GeoHashCompress {
-    constructor(compressedHashes: string[], maxPrecision: number = 7, minPrecision: number = 1);
-    contains(long: number, lat: number): boolean;
-    containsHash(hash: string): boolean;
-    set: Set<string>;
-    maxPrecision: number;
-    minPrecision: number;
-    toGeoJson(): any;
-  }
-
-  declare const geoHashCompressFromPoly = async (polygon: CoordinatesArray, precision: number, minPrecision: number) => GeoHashCompress
-  declare const buildCompressedHashSet = async (polygon: CoordinatesArray, precision: number, minPrecision: number) => GeoHashCompress
-  declare const encode = (lng: number, lat: number, hashLength: number) => string
+  // input, path of [[lng,lat]] points representing the line, and a thickness amount in meters
+  declare const geolineToRectangle = async (path: GeopointPath, thickness: number) => GepointArray
 }
