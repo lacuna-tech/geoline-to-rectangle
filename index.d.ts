9540c453403ebc670e92b3f3da906c10e8f284b7
ethan sherr <>
2022-02-28 13:31:10 -0500

    fix

--- index.d.ts
+++ index.d.ts
@@ -4,5 +4,5 @@ export module '@lacuna/geoline-to-rectangle' {
   declare type GepointArray = Array<Array<number>>
 
   // input, path of [[lng,lat]] points representing the line, and a thickness amount in meters
-  declare const geolineToRectangle = async (path: GeopointPath, thickness: number) => GepointArray
+  declare const geolineToRectangle = async (path: GepointArray, thickness: number) => GepointArray
 }
